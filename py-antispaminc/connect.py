d914eff9d8dc47adc2d28e7f696af9208b65b82e
StarkGang <StarkGangZ@gmail.com>
2020-12-04 19:12:33 +0530

    Update connect.py

--- py-antispaminc/connect.py
+++ py-antispaminc/connect.py
@@ -11,9 +11,39 @@ class Antispaminc():
     '''
     LoL
     '''
-    def __init__(self, api_key=None):
-          self.api_key = api_key
+    def __init__(self, token=None):
+          self.token = token
           self.base = 'http://antispaminc.tk/'
-          if api_key is None:
+          if token is None:
               raise ApiError("You must supply a valid AntispamInc Api Key, Contact @AntispamIncBot To Get One")
     
+    def is_banned(self, user_id):
+          token = self.token  
+          userid = user_id
+          is_it = {
+              'token': token,
+              'userid': userid
+          }
+          urlz = self.base + '/info/'
+          seds = requests.post(url=urlz, data=is_it).json()
+          if seds['error'] == True:
+                raise ApiError('Something Went Wrong. \nError' + seds['full'])
+          else:
+                lol = seds['banned']
+                return lol
+        
+    def reason_s(self, user_id):
+          token = self.token  
+          userid = user_id
+          is_it = {
+              'token': token,
+              'userid': userid
+          }
+          urlz = self.base + '/info/'
+          seds = requests.post(url=urlz, data=is_it).json()
+          if seds['error'] == True:
+                raise ApiError('Something Went Wrong. \nError' + seds['full'])
+          else:
+                lol = seds['reason']
+                return lol
+          

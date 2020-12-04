a16ee84814675ba00aeb173642b4c186c87c61fd
StarkGang <StarkGangZ@gmail.com>
2020-12-04 19:14:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="AntispamInc", 
     version="0.0.1",
-    author="Example Author",
+    author="Midhun KM",
     url="antispaminc.tk",
     author_email="dev@antispaminc.tk",
     description="Python Wrapper For Antispam Inc",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=[
-            'requests',
-        ],
+    install_requires=["requests >= 2.22.0"])
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

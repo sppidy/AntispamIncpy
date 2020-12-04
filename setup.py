ffe8d0927a74e149bd90d84357289f1a53e497ad
StarkGang <StarkGangZ@gmail.com>
2020-12-04 22:13:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyantispaminc", 
-    version="0.0.1",
+    version="0.0.2",
     author="Midhun KM",
     url="antispaminc.tk",
     author_email="dev@antispaminc.tk",

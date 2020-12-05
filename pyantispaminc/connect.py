18822cd08545fc03e6f3fbb62964dcee42ebbec9
StarkGang <StarkGangZ@gmail.com>
2020-12-05 09:57:53 +0530

    Update connect.py

--- pyantispaminc/connect.py
+++ pyantispaminc/connect.py
@@ -2,45 +2,33 @@ import json
 import os
 from datetime import datetime, timedelta
 import requests
-from class_types import anc
+from .class_types import Antispamclass
 
 class Antispaminc():
     '''
     LoL
     '''
+
     def __init__(self, token=None):
-          self.token = token
-          self.base = 'http://antispaminc.tk/'
-          if token is None:
-              raise ApiError("You must supply a valid AntispamInc Api Key, Contact @AntispamIncBot To Get One")
-    
+        self.token = token
+        self.base = 'http://antispaminc.tk/'
+        if token is None:
+            raise ApiError("You must supply a valid AntispamInc Api Key, Contact @AntispamIncBot To Get One")
+
     def is_banned(self, user_id):
-          token = self.token  
-          userid = user_id
-          is_it = {
-              'token': token,
-              'userid': userid
-          }
-          urlz = self.base + '/info/'
-          seds = requests.post(url=urlz, data=is_it).json()
-          if seds['error'] == True:
-                raise ApiError('Something Went Wrong. \nError' + seds['full'])
-          else:
-                lol = seds['banned']
-                return lol
-        
-    def reason_s(self, user_id):
-          token = self.token  
-          userid = user_id
-          is_it = {
-              'token': token,
-              'userid': userid
-          }
-          urlz = self.base + '/info/'
-          seds = requests.post(url=urlz, data=is_it).json()
-          if seds['error'] == True:
-                raise ApiError('Something Went Wrong. \nError' + seds['full'])
-          else:
-                lol = seds['reason']
-                return anc(lol)
-          
+        token = self.token
+        userid = user_id
+        is_it = {
+            'token': token,
+            'userid': userid
+        }
+        urlz = self.base + '/info/'
+        seds = requests.post(url=urlz, data=is_it).json()
+        if seds['error'] == True:
+            raise ApiError('Something Went Wrong. \nError' + seds['full'])
+        else:
+            lol = seds['banned']
+            return Antispamclass(**seds)
+
+class ApiError(Exception):
+    pass

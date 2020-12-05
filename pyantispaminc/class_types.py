703c245a3f068206558b967c3765edb44e2ea6a3
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-12-05 10:52:41 +0530

    Added License

--- pyantispaminc/class_types.py
+++ pyantispaminc/class_types.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2020 AntiSpam, Inc. <http://antispaminc.tk/>
+#
+#                    GNU GENERAL PUBLIC LICENSE
+#                      Version 3, 29 June 2007
+
+# Copyright (C) 2020 AntiSpam, Inc. <http://antispaminc.tk/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
 from enum import Enum, auto
 from typing import Optional
 
